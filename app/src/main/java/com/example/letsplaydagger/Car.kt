eb306ced3b67e8e576195d08138db9080f19737e
RubensSoft <joserubencastrosoriano@gmail.com>
2022-12-30 16:28:34 +0100

    add a method injection sample, it'll be called after the inject constructor, the order of inject is constructor, field, method

--- app/src/main/java/com/example/letsplaydagger/Car.kt
+++ app/src/main/java/com/example/letsplaydagger/Car.kt
@@ -3,10 +3,18 @@ package com.example.letsplaydagger
 import android.util.Log
 import javax.inject.Inject
 
-class Car @Inject constructor(val engine: Engine, val wheels: Wheels) {
+class Car @Inject constructor(
+    val engine: Engine,
+    val wheels: Wheels
+) {
 
     companion object {
         private const val TAG = "Car"
+    }
+
+    @Inject
+    fun enableRemoteControl(remote: RemoteControl) {
+        remote.setListener(this)
     }
 
     fun drive() {
---
+++ app/src/main/java/com/example/letsplaydagger/RemoteControl.kt
@@ -0,0 +1,15 @@
+package com.example.letsplaydagger
+
+import android.util.Log
+import javax.inject.Inject
+
+class RemoteControl @Inject constructor() {
+    companion object {
+        private const val TAG = "Car"
+    }
+
+    fun setListener(car: Car) {
+        Log.d(TAG, "Remote control connected to $car")
+    }
+
+}
